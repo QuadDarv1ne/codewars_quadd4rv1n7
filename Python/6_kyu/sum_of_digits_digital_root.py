9c44a25e6a8b82a44315df33595bcfbac111eae6
Max Dupley <maksimqwe42@mail.ru>
2024-11-29 21:43:46 +0300

    new file:   Python/6_kyu/sum_of_digits_digital_root.py

---
+++ Python/6_kyu/sum_of_digits_digital_root.py
@@ -0,0 +1,22 @@
+'''
+[ Sum of Digits / Digital Root ]
+
+Digital root is the recursive sum of all the digits in a number.
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples:
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+'''
+
+def digital_root(n):
+    while n >= 10:  # Continue until the number is a single digit
+        n = sum(int(digit) for digit in str(n))  # Sum the digits
+    return n
+
+
+# TODO: Заметки
+## Автор: Дуплей Максим Игоревич / Dupley Maxim Igorevich
+## Дата: 29.11.2024
