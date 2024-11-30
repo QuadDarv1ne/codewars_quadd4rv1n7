bbeb0809f22d11e76bd44d663d76615c225c45b7
Max Dupley <maksimqwe42@mail.ru>
2024-11-30 13:33:12 +0300

    new file:   Python/8_kyu/grasshopper_summation.py 	new file:   Python/8_kyu/string_repeat.py

---
+++ Python/8_kyu/string_repeat.py
@@ -0,0 +1,18 @@
+'''
+[ String repeat ]
+
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output):
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+'''
+
+
+def repeat_str(repeat, string):
+    return string * repeat
+
+
+# TODO: Заметки
+## Автор: Дуплей Максим Игоревич / Dupley Maxim Igorevich
+## Дата: 29.11.2024
