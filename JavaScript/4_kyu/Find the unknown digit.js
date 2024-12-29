5db8d07ebbf316f4f52e7be2bfb5782d9c1f4699
Max Dupley <51045274+QuadDarv1ne@users.noreply.github.com>
2024-12-29 13:19:51 +0300

    Update Find the unknown digit.js

--- JavaScript/4_kyu/Find the unknown digit.js
+++ JavaScript/4_kyu/Find the unknown digit.js
@@ -1,36 +1,67 @@
+/**
+ * Функция решает задачу нахождения значения неизвестного знака (?), который заменяется на цифру от 0 до 9.
+ * Входное выражение представляет собой математическое уравнение, в котором одна или несколько цифр заменены на знак вопроса (?).
+ * Мы должны найти минимальную возможную цифру для знака вопроса, чтобы уравнение стало истинным.
+ * 
+ * Алгоритм:
+ * 1. Заменяет символ `=` на `==`, чтобы уравнение стало валидным для проверки с помощью функции `eval()`.
+ * 2. Обрабатывает случаи с двойным минусом (`--`), заключая их в скобки для корректной работы с `eval()`.
+ * 3. Для каждой цифры от 0 до 9 заменяет все знаки вопроса на текущую цифру и проверяет корректность выражения.
+ * 4. Для цифры 0 дополнительно проверяет, чтобы не было некорректных чисел с ведущими нулями (например, 012).
+ * 5. Если выражение становится истинным, возвращает найденную цифру. Если ни одна цифра не подходит, возвращает -1.
+ * 
+ * @param {string} exp - Входное строковое математическое выражение с неизвестными знаками (?), которые нужно заменить на цифры.
+ * @returns {number} - Минимальная цифра, которая заменяет все знаки вопроса и делает выражение истинным, или -1, если решения нет.
+ */
+function solveExpression(exp) {
+  console.log('------------------new------------------');
+  console.log(exp);
+  let op = '+-*=';
+  let arr = []
+  //  making 'exp' to an actual conditional expression
+  exp = exp.replace('=','==');
+  //  '--' is not acceptable on 'eval', so parenthesize them.
+  if((idx = exp.indexOf('--')) !== -1){
+    let idx2 = exp.indexOf('==');
+    let temp = exp.split``;
+    temp.splice(idx+1, 0, '(');
+    temp.splice(idx2+1, 0, ')');
+    exp = temp.join``;
+  }
+  
+  //  replacing '?' to 0 -> 9
+  console.log(exp);
+  for(let i = 0; i <= 9; i++){
+    let temp = exp.replace(/\?/g, i);
+    
+    //  when it's 0, there're more things to check
+    if(i === 0){
+      let flag = false;
+      for(let j = 0; j < temp.length; j++){
+        console.log('inner',j);
+        if(temp[j] == 0 && (j-1 < 0 || op.indexOf(temp[j-1]) !== -1) && !(j+1 >= temp.length || op.indexOf(temp[j+1]) !== -1)){
+          flag = true;
+          break;
+        }
+      }
+      console.log('flag',flag);
+      if(flag) continue;
+    }
+    
+    //  check if the new expression is correct.
+    console.log(temp);
+    if(exp.indexOf(String(i)) === -1 && eval(temp)){
+      console.log('answer', i);
+      return i;
+    }
+  }
+  
+  //  if all failed, nothing is possible.
+  return -1;
+}
+
 /*
-[ Find the unknown digit ]
-[  ]
-
-To give credit where credit is due:
-This problem was taken from the ACMICPC-Northwest Regional Programming Contest.
-Thank you problem writers.
-
-You are helping an archaeologist decipher some runes.
-He knows that this ancient society used a Base 10 system, and that they never start a number with a leading zero.
-He's figured out most of the digits as well as a few operators, but he needs your help to figure out the rest.
-
-The professor will give you a simple math expression, of the form
-
-[number][op][number]=[number]
-He has converted all of the runes he knows into digits.
-The only operators he knows are addition (+),subtraction(-), and multiplication (*), so those are the only ones that will appear.
-Each number will be in the range from -1000000 to 1000000, and will consist of only the digits 0-9, possibly a leading -, and maybe a few ?s.
-If there are ?s in an expression, they represent a digit rune that the professor doesn't know (never an operator, and never a leading -).
-All of the ?s in an expression will represent the same digit (0-9), and it won't be one of the other given digits in the expression.
-No number will begin with a 0 unless the number itself is 0, therefore 00 would not be a valid number.
-
-Given an expression, figure out the value of the rune represented by the question mark.
-If more than one digit works, give the lowest one.
-If no digit works, well, that's bad news for the professor - it means that he's got some of his runes wrong. output -1 in that case.
-
-Complete the method to solve the expression to find the value of the unknown rune.
-The method takes a string as a paramater repressenting the expression and will return an int value representing the unknown rune or -1 if no such rune exists.
+Хижина программиста Æ 
+https://t.me/hut_programmer_07  
+@quadd4rv1n7
 */
-
-
-
-
-
-// Автор: Дуплей Максим Игоревич / Dupley Maxim Igorevich
-// Дата: 16.12.2024
