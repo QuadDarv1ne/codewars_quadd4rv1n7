1b95cc4ed29b77725d8473dcce137def1859c851
Max Dupley <51045274+QuadDarv1ne@users.noreply.github.com>
2024-12-29 13:32:46 +0300

    Create MakeUpperCase.js

---
+++ JavaScript/8_kyu/MakeUpperCase.js
@@ -0,0 +1,16 @@
+/**
+ * Преобразует переданную строку в верхний регистр.
+ * 
+ * @param {string} str - Строка, которую необходимо преобразовать в верхний регистр.
+ * @returns {string} Строка в верхнем регистре.
+ */
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+/*
+Хижина программиста Æ 
+https://t.me/hut_programmer_07  
+@quadd4rv1n7
+*/
