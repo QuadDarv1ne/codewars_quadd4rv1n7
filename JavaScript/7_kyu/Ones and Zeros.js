fcbc0fb7e42c577e6604bf2a401965fb8cffe321
Max Dupley <51045274+QuadDarv1ne@users.noreply.github.com>
2024-12-29 13:12:39 +0300

    Create Ones and Zeros.js

---
+++ JavaScript/7_kyu/Ones and Zeros.js
@@ -0,0 +1,10 @@
+const binaryArrayToNumber = arr => {
+  // Преобразуем массив в строку и передаем в parseInt с основанием 2 для получения числа
+  return parseInt(arr.join(''), 2);
+};
+
+/*
+Хижина программиста Æ 
+https://t.me/hut_programmer_07  
+@quadd4rv1n7
+*/
